edd62606dd3abb22096859bfe69fe31fe016e9f6
darkwizard242 <am900820@gmail.com>
2021-12-25 13:17:34 -0500

    molecule testinfra python functions for syft role

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,38 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+PACKAGE = 'syft'
+PACKAGE_BINARY = "/usr/local/bin/syft"
+
+
+def test_syft_package_installed(host):
+    """
+    Tests if syft package is in installed state.
+    """
+    assert host.package(PACKAGE).is_installed
+
+
+def test_syft_binary_exists(host):
+    """
+    Tests if syft binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
+
+
+def test_syft_binary_file(host):
+    """
+    Tests if syft binary is a file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
+
+
+def test_syft_binary_which(host):
+    """
+    Tests the output to confirm syft's binary location.
+    """
+    assert host.check_output('which syft') == PACKAGE_BINARY
